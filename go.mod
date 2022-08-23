11ced9b7e9808670978f0b5f1810ff896e53d326
ucho456 <watapqxyz@gmail.com>
2022-08-23 14:46:40 +0900

    Send a sample message to LINE

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ucho456/go_weather
 
 go 1.16
+
+require github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
-package GoWeather
+package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
 
-func GoWeather() {
-	fmt.Println("Hello world")
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	GoWeather(context.TODO())
+}
+
+func GoWeather(ctx context.Context) error {
+	bot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := "Hello world"
+
+	message := linebot.NewTextMessage(result)
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+	return nil
 }
